fed88578aaeb15d883a7f6b5dff043ae0e184d76
xuwuli <xuwuli98@163.com>
2020-12-16 21:52:44 +0800

    C110,111:二叉树的深度

--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -1,16 +1,42 @@
 package tree;
 
+//public class TreeNode {
+
+//	int val;
+//	TreeNode left;
+//	TreeNode right;
+//
+//	TreeNode(int x) {
+//		val = x;
+//	}
+//	
+//	public static TreeNode createTree(int[] nodeVals) {
+//		TreeNode root = new TreeNode(1);
+//		return root; 
+//	}
+//}
+
+/**
+ * 平衡二叉树
+ * 
+ * 使用的题目：C110\C111
+ * 
+ */
 public class TreeNode {
 	int val;
 	TreeNode left;
 	TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
+	TreeNode() {
 	}
-	
-	public static TreeNode createTree(int[] nodeVals) {
-		TreeNode root = new TreeNode(1);
-		return root; 
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
 	}
 }
